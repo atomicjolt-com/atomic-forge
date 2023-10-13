e9c6781a13859234428a38c481da21967c4f8a1f
Justin Ball <justinball@gmail.com>
2023-10-13 00:20:58 -0600

    Added tests for jwks handler

--- atomic-lti-tool/src/handlers/jwks.rs
+++ atomic-lti-tool/src/handlers/jwks.rs
@@ -14,58 +14,69 @@ pub async fn jwks(key_store: &dyn KeyStore) -> Result<HttpResponse, AtomicToolEr
   )
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
-//   use atomic_lti::{errors::SecureError, jwks::KeyStore};
-//   use openssl::rsa::Rsa;
+#[cfg(test)]
+mod tests {
+  use std::vec;
 
-//   struct MockKeyStore {}
+  use super::*;
+  use actix_web::http;
+  use atomic_lti::{
+    errors::SecureError,
+    jwks::{Jwks, KeyStore},
+    secure::generate_rsa_key_pair,
+  };
+  use openssl::rsa::Rsa;
 
-//   impl KeyStore for MockKeyStore {
-//     fn get_current_keys(&self) -> Result<Vec<Rsa<openssl::pkey::Private>>, SecureError> {
-//       todo!()
-//     }
-//   }
+  struct MockKeyStore {}
 
-//   #[actix_web::test]
-//   async fn returns_jwks_with_valid_key_store() {
-//     let key_store = MockKeyStore {};
-//     let resp = jwks(&key_store).await.unwrap();
+  impl KeyStore for MockKeyStore {
+    fn get_current_keys(&self) -> Result<Vec<Rsa<openssl::pkey::Private>>, SecureError> {
+      let jwks_passphrase = "fake-passphrase-for-jwks-test";
+      let (rsa_key_pair, _) =
+        generate_rsa_key_pair(jwks_passphrase).expect("Failed to generate key pair");
 
-//     assert_eq!(resp.status(), http::StatusCode::OK);
-//     assert_eq!(
-//       resp.headers().get("content-type").unwrap(),
-//       "application/json"
-//     );
+      Ok(vec![rsa_key_pair])
+    }
+  }
 
-//     let body = test::read_body(resp).await;
-//     let jwks: JWK = serde_json::from_slice(&body).unwrap();
+  #[actix_web::test]
+  async fn returns_jwks_with_valid_key_store() {
+    let key_store = MockKeyStore {};
+    let resp = jwks(&key_store)
+      .await
+      .expect("Request to jwks handler failed");
 
-//     assert_eq!(jwks.kty, "RSA");
-//     assert_eq!(jwks.n, "n");
-//     assert_eq!(jwks.e, "e");
-//     assert_eq!(jwks.d, Some("d".to_string()));
-//     assert_eq!(jwks.p, Some("p".to_string()));
-//     assert_eq!(jwks.q, Some("q".to_string()));
-//     assert_eq!(jwks.dp, Some("dp".to_string()));
-//     assert_eq!(jwks.dq, Some("dq".to_string()));
-//     assert_eq!(jwks.qi, Some("qi".to_string()));
-//   }
+    assert_eq!(resp.status(), http::StatusCode::OK);
+    assert_eq!(
+      resp.headers().get("content-type").unwrap(),
+      "application/json"
+    );
 
-//   #[actix_web::test]
-//   async fn returns_internal_server_error_with_invalid_key_store() {
-//     struct InvalidKeyStore {}
+    let body = actix_web::body::to_bytes(resp.into_body()).await.unwrap();
+    let jwks: Jwks = serde_json::from_slice(&body).unwrap();
 
-//     impl KeyStore for InvalidKeyStore {
-//       fn get_current_keys(&self) -> Result<Vec<Rsa<openssl::pkey::Private>>, SecureError> {
-//         Err(SecureError::PrivateKeyError("No private key found"))
-//       }
-//     }
+    assert_eq!(jwks.keys[0].kty, "RSA");
+    assert!(!jwks.keys[0].n.is_empty());
+    assert_eq!(jwks.keys[0].e, "AQAB");
+  }
 
-//     let key_store = InvalidKeyStore {};
-//     let resp = jwks(&key_store).await.unwrap();
+  #[actix_web::test]
+  async fn returns_internal_server_error_with_invalid_key_store() {
+    struct InvalidKeyStore {}
 
-//     assert_eq!(resp.status(), http::StatusCode::INTERNAL_SERVER_ERROR);
-//   }
-// }
+    impl KeyStore for InvalidKeyStore {
+      fn get_current_keys(&self) -> Result<Vec<Rsa<openssl::pkey::Private>>, SecureError> {
+        Err(SecureError::EmptyKeys)
+      }
+    }
+
+    let key_store = InvalidKeyStore {};
+    let result = jwks(&key_store).await;
+
+    assert!(result.is_err());
+    assert_eq!(
+      result.unwrap_err().to_string(),
+      "Unable to generate a new JWK: There are currently no keys available"
+    );
+  }
+}
